26af702378232a3389a055f6e25565aa8c8efb73
Polina Bodnar <honey@list.ru>
2025-03-31 13:56:26 +0200

    Изменения

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Четное или не четное
 
+        // Четное или не четное
         int i;
         for (i = 0; i <= 10; i++)
             if (i % 2 == 0) {
