93aa48a8affcbca8d2d61e7ce094f5d1cbb4af59
Polina Bodnar <honey@list.ru>
2025-04-01 14:24:30 +0200

    Правки от 1.04 Оставлены комментарии. Весь код запускается.

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,30 @@ public class Main {
         /* Дано число. Напишите программу, которая определяет, является ли оно четным или нечетным. */
         int op = 5;
         if (op % 2 == 0) {
-                System.out.println(op + " - Число четное");
-            } else {
-                System.out.println(op + " - Число нечетное");
-            }
+            System.out.println(op + " - Число четное");
+        } else {
+            System.out.println(op + " - Число нечетное");
+        }
 
         /* Даны два числа. Выведите «Первое больше», «Второе больше» или «Равны». */
         int p1 = 20;
         int p2 = 30;
         if (p1 > p2) {
-            System.out.println("Первое больше");
+            System.out.println("Сравнение чисел: Первое больше");
         } else if (p1 < p2) {
-            System.out.println("Второе больше");
+            System.out.println("Сравнение чисел: Второе больше");
         } else {
-            System.out.println("Равны");
+            System.out.println("Сравнение чисел: Равны");
         }
 
         /* Дано число. Выведите «Положительное», «Отрицательное» или «Равно нулю». */
         int e = 11;
         if (e > 0) {
-            System.out.println("Положительное");
+            System.out.println("Число: Положительное");
         } else if (e < 0) {
-            System.out.println("Отрицательное");
+            System.out.println("Число: Отрицательное");
         } else {
-            System.out.println("Равно нулю");
+            System.out.println("Число: Равно нулю");
         }
 
         /* Дано число (возраст). Если возраст >= 18, выведите «Доступ разрешен», иначе — «Доступ запрещен. */
@@ -38,53 +38,53 @@ public class Main {
             System.out.println("Доступ запрещен");
         }
 
-        /* Придумать задачу. Задача: Дано число. Проверить, попадает ли число оно в диапазон от 10 до 20  */
+        /* Придумать задачу. Задача: Дано число. Проверить, попадает ли число в диапазон от 10 до 20  */
         int num7 = 15;
         if (num7 >= 10 && num7 <= 20) {
-            System.out.println("Число в диапазоне от 10 до 20.");
+            System.out.println("Число в диапазоне от 10 до 20");
         } else {
-            System.out.println("Число вне диапазона.");
+            System.out.println("Число вне диапазона");
         }
 
         /* Тернарный оператор. Дано число. Используя тернарный оператор, определите, положительное, отрицательное или нулевое */
         int gg = 3;
-        System.out.println((gg > 0) ? "Положительное" : (gg < 0) ? "Отрицательное" : "Ноль");
+        System.out.println((gg > 0) ? "Определение числа: Положительное" : (gg < 0) ? "Определение числа: Отрицательное" : "Определение числа: Ноль");
 
         /* Тернарный оператор. Дано число. С помощью тернарного оператора проверьте, делится ли оно на 3 и 5 одновременно. Выведите «Кратно» или «Не кратно» */
         int n1 = 7;
-        System.out.println((n1 % 3 == 0) || (n1 % 5 == 0) ? "Кратно" : "Не кратно");
+        System.out.println((n1 % 3 == 0) && (n1 % 5 == 0) ? "Провека. Число: Кратно" : "Провека. Число: Не кратно");
 
         /* Тернарный оператор. Для заданного числа верните строку «Четное» или «Нечетное», используя тернарный оператор. */
         int n2 = 7;
-        System.out.println((n2 % 2 == 0) ? "Четное" : "Нечетное");
+        System.out.println((n2 % 2 == 0) ? "Заданое число: четное" : "Заданое число: нечетное");
 
         /* Дано число от 1 до 7. Выведите название дня недели. Если число вне диапазона, выведите «Ошибка» */
         int day = 4;
         switch (day) {
             case 1:
-                System.out.println("ПН");
+                System.out.println("Выбран день недели: ПН");
                 break;
             case 2:
-                System.out.println("ВТ");
+                System.out.println("Выбран день недели: ВТ");
                 break;
             case 3:
-                System.out.println("СР");
+                System.out.println("Выбран день недели: СР");
                 break;
             case 4:
-                System.out.println("ЧТ");
+                System.out.println("Выбран день недели: ЧТ");
                 break;
             case 5:
-                System.out.println("ПТ");
+                System.out.println("Выбран день недели: ПТ");
                 break;
             case 6:
-                System.out.println("Cб");
+                System.out.println("Выбран день недели: Cб");
                 break;
             case 7:
-                System.out.println("ВС");
+                System.out.println("Выбран день недели: ВС");
                 break;
             default:
                 System.out.println("Ошибка");
-            }
+        }
 
         /*  Дано число от 1 до 5. Выведите описание оценки (оцените свою работу) */
         int score = 3;
@@ -109,29 +109,28 @@ public class Main {
         }
 
         /* Дан список чисел. Посчитайте сумму всех четных элементов */
-        int[] numbers6 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int sum6 = 0;
-        for (int num6 : numbers6) {
-            if (num6 % 2 == 0) {
-                sum6 += num6;
+        for (int x = 0; x < 10; x++) {
+            if (x % 2 == 0) {
+                sum6 += x;
             }
         }
-        System.out.println(sum6);
+        System.out.println("Сумма всех четных элементов: " + sum6);
 
         /*  Дан список чисел. Найдите максимальное значение */
-        int[] numbers5 = {10, 2, 3, 11, 13, 12, 22, 19};
-        int max = numbers5[0];
-        for (int numy : numbers5) {
-            if (numy > max) {
-                max = numy;
-            }
+        int max = Integer.MIN_VALUE;
+        for (int z = 0; z < 10; z++) {
+             if (z > max) ; {
+                 max = z;
+             }
         }
-        System.out.println(max);
+        System.out.println("Максимальное значение: " + max);
+
 
         /* Дано число n. Выведите таблицу умножения для n от 1 до 10 */
         int nt = 2;
         for (int k = 1; k <= 10; k++) {
-            System.out.printf("%d x %d = %d%n", nt, k, nt * k);
+             System.out.printf("%d x %d = %d%n", nt, k, nt * k);
         }
 
         /* С помощью цикла for выведите числа от 10 до 1 в обратном порядке */
@@ -141,26 +140,26 @@ public class Main {
 
         /* Найдите количество четных чисел в диапазоне от 1 до 50 */
         int count = 0;
-        for (int h = 1; h<= 50; h++) {
+        for (int h = 1; h <= 50; h++) {
             if (h % 2 == 0) {
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("Количество четных в диапазоне 1 до 50: " + count);
 
         /* Найдите наименьшее число больше 100, которое делится на 7 */
         int number3 = 100;
         int result = (number3 / 7 + 1) * 7;
-        System.out.println(result);
+        System.out.println("Наибольшее число после 100, делящиеся на 7: " + result);
 
         /* Дано число n. Вычислите его факториал через цикл while */
         int f = 7;
         long factorial = 1;
         while (f > 1) {
-            factorial *= f;
-            f--;
-        }
-        System.out.println(factorial);
+           factorial *= f;
+             f--;
+         }
+        System.out.println("Значение фракториала: " + factorial);
 
         /* Дано число. Определите, является ли оно простым */
         int n = 7;
@@ -181,7 +180,7 @@ public class Main {
         /* Придумать задачу самим. Задача: Выведи числа, начиная с 100 и уменьшая их на 25 в каждом цикле, пока переменная не станет меньше или равно 5 */
         int c = 100;
         while (c > 5) {
-            System.out.println("Число: " + c);
+            System.out.println( c + " уменьшается на 25");
             c -= 25;
         }
 
@@ -192,17 +191,16 @@ public class Main {
             System.out.println(b);
         }
 
-         /* Дан список чисел. Суммируйте элементы, пока не встретите отрицательное число */
-         int[] numbers1 = {1, 2, 3, 4, 5, -1, 6, 7};
-         int sumt = 0;
-
-         for (int numt : numbers1) {
-             if (numt < 0) {
-                 break;
-             }
-             sumt += numt;
-         }
-             System.out.println(sumt);
+        /* Дан список чисел. Суммируйте элементы, пока не встретите отрицательное число */
+        int[] numbers1 = {1, 2, 3, 4, 5, -1, 6, 7};
+        int sumt = 0;
+        for (int numt : numbers1) {
+            if (numt < 0) {
+                break;
+            }
+            sumt += numt;
+        }
+        System.out.println("После отрицательнго числа, сумма равна: " + sumt);
 
         /* Дан список чисел. Суммируйте элементы, пока сумма не превысит 100 */
         int[] numbers2 = {20, 30, 25, 10, 50, 60, 15};
@@ -213,8 +211,9 @@ public class Main {
                 break;
             }
         }
-        System.out.println(sums);
+        System.out.println("Минимальная сумма после 100: " + sums);
 
+        }
     }
-}
+
 
